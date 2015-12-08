5329af702d6d388897fbe849bbcaad01dccce93b
Kamil Kurek <kamil.kurek@candf.com>
2015-12-08 16:47:14 +0100

    updated the attribute tester

--- src/test/java/pl/tecna/xml/AttributeMergerTest.java
+++ src/test/java/pl/tecna/xml/AttributeMergerTest.java
@@ -9,6 +9,7 @@ import javax.xml.parsers.DocumentBuilder;
 
 import org.jukito.JukitoRunner;
 import org.jukito.UseModules;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.w3c.dom.Element;
@@ -33,8 +34,18 @@ public class AttributeMergerTest {
     Element test1Element = readDocumentElement(TEST_FILE_1_NAME);
     Element test2Element = readDocumentElement(TEST_FILE_2_NAME);
     attributeMerger.mergeAttributes(test1Element, test2Element, test2Element);
+    testAttribute(test2Element, "attribute1", "TEST2");
+    Assert.assertFalse(test2Element.hasAttribute("attribute2"));
+    testAttribute(test2Element, "attribute3", "TEST3");
+    testAttribute(test2Element, "attribute4", "TEST4");
+    Assert.assertFalse(test2Element.hasAttribute("attribute5"));
   }
 
+  private void testAttribute(Element element, String name, String value) {
+    Assert.assertTrue(element.hasAttribute(name));
+    Assert.assertEquals(value, element.getAttribute(name));
+  }
+  
   private Element readDocumentElement(String fileName) throws IOException {
     try {
       return documentBuilderProvider.get().parse(
